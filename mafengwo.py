9a142c7246307a0de47623a73a7417f8f2cd99b5
ganzi01234 <229637012@qq.com>
2018-12-28 14:52:43 +0800

    Update mafengwo.py
    升级马蜂窝的爬虫，经常遇到各种http错误。估计是触发了反扒策略。

--- mafengwo.py
+++ mafengwo.py
@@ -6,52 +6,56 @@
 from pyspider.libs.base_handler import *
 from pyspider.libs.header_switch import HeadersSelector
 from pyquery import PyQuery as pq
+from pyspider.libs.utils import md5string
+from random import choice
 import json
+import time
+import re
 
 main_url = "http://www.mafengwo.cn/"
+
 class Handler(BaseHandler):
+    file = open("/usr/lib/python2.7/site-packages/pyspider/proxy_ip.txt","r")
+    filecontent = file.readlines()
+    
+    header_slt = HeadersSelector()
+    header = header_slt.select_header()  # èŽ·å–ä¸€ä¸ªæ–°çš„ header
+    header["X-Requested-With"] = "XMLHttpRequest"
+    header["Host"] = "pagelet.mafengwo.cn"
+    
     crawl_config = {
-        'itag': 'v1',
-        "user_agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36",
+        "itag":"v12",
+        #"proxy":choice(filecontent).strip('\r\n'),
         "timeout": 120,
         "connect_timeout": 60,
         "retries": 5,
         "auto_recrawl": True,
+        "process_time_limit":120
     }
+    
+    #def get_taskid(self,task):
+        #return md5string(task['url']+json.dumps(task['fetch'].get('data','')))
 
-    #@every(minutes=1)
+    @every(minutes=24*60)
+    @config(age=10*60*60)
     def on_start(self):
-        header_slt = HeadersSelector()
-        header = header_slt.select_header()  # »ñÈ¡Ò»¸öÐÂµÄ header
-        # header["X-Requested-With"] = "XMLHttpRequest"
-        header["Host"] = "www.mafengwo.cn"
-        self.crawl('http://www.mafengwo.cn/', callback=self.index_page,
-                   headers=header)
-
-    @config(age=1)
-    def index_page(self, response):
-        header_slt = HeadersSelector()
-        header = header_slt.select_header()  # »ñÈ¡Ò»¸öÐÂµÄ header
-        # header["X-Requested-With"] = "XMLHttpRequest"
-        header["Host"] = "www.mafengwo.cn"
-        if response.cookies:
-            header["Cookies"] = response.cookies
-        for each in response.doc('a[href^="http://www.mafengwo.cn/traveller/"]').items():
-            self.crawl(each.attr.href, callback=self.tdetail_page,
-                  headers=header)
-        for each in response.doc('a[href^="http://www.mafengwo.cn/i/"]').items():
-            self.crawl(each.attr.href, callback=self.idetail_page,
-                   headers=header)
-            
-        base_url = "http://pagelet.mafengwo.cn/note/pagelet/recommendNoteApi?callback=jQuery18108796192355544348_1545187965755&params=%7B%22type%22%3A0%2C%22objid%22%3A0%2C%22page%22%3A"
-        final_url="%2C%22ajax%22%3A1%2C%22retina%22%3A1%7D&_=1545191377450"
+        base_url = "http://pagelet.mafengwo.cn/note/pagelet/recommendNoteApi?callback=jQuery18106975500853167276_1545707077976&params=%7B%22type%22%3A0%2C%22objid%22%3A0%2C%22page%22%3A"
+        final_url="%2C%22ajax%22%3A1%2C%22retina%22%3A1%7D&_=1545707093353"
             
         for page_num in range(1, 90):
+            time.sleep(1)
             page_url = base_url + '{}&'.format(page_num) + final_url
             #print page_url
-            self.crawl(page_url, callback=self.parse_page, method='GET')
-            
-    def parse_page(self, response):
+            self.crawl(page_url, callback=self.index_page)
+
+    @config(age=10*60*60)
+    def index_page(self, response): 
+        #header_slt = HeadersSelector()
+        #header = header_slt.select_header()  # èŽ·å–ä¸€ä¸ªæ–°çš„ header
+        # header["X-Requested-With"] = "XMLHttpRequest"
+        #header["Host"] = "www.mafengwo.cn"
+        #if response.cookies:
+            #header["Cookies"] = response.cookies
         #print response.text
         nPos=response.text.index("(")+1
         nEnd=response.text.rfind(")")
@@ -63,10 +67,10 @@ class Handler(BaseHandler):
         d = pq(params['data']['html'])
         for each in d('a[href^="\/i"]').items():
             url = each.attr.href
-            print main_url + url
-            self.crawl(main_url +url, callback=self.idetail_page, method='GET')
+            print main_url + url[1:]
+            self.crawl(main_url +url[1:]+"?static_url=true", callback=self.idetail_page)
 
-    @config(priority=2)
+    @config(priority=3)
     def idetail_page(self, response):
         print response
         cost = response.doc('.cost').text()
@@ -84,17 +88,4 @@ class Handler(BaseHandler):
             'content': content
         }
     
-    @config(priority=1)
-    def tdetail_page(self, response):
-        title = response.doc('.h1').text()
-        content = response.doc('.ac_body > p').text()
-        return {
-            "url": response.url,
-            "title": response.doc('title').text(),
-            'cost': '',
-            'time': '',
-            'day': '',
-            'people': '',
-            "content":content
-        }
-
+    
